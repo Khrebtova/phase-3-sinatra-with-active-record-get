e87debbfed4ce2308dcc46d9a16487910fe70a34
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-04-26 15:54:39 -0700

    commit tree d0a0b74e216fa748753b51c9ff161e73c6a420c2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,27 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
   get '/' do
-    { message: "Hello world" }.to_json
+    { message: "Hello world!" }.to_json
+  end
+
+  get '/games' do
+    # get all the games from the database
+    games = Game.all.order(:title).limit(10)
+    # return a JSON response with an array of all the game data
+    games.to_json
+  end
+
+  get '/games/:id' do
+    # get the game from the database
+    game = Game.find(params[:id])
+    # return a JSON response with the game data
+    game.to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: { only: [:comment, :score], include: {
+        user: { only: [:name] }
+      } }
+    })
+
   end
 
 end
